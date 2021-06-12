aa85d13271474d0804e7ed3a9d20f8e350ae4206
Katyusha <74827754+HizMori@users.noreply.github.com>
2021-06-12 20:19:02 +0500

    bot

--- bot.py
+++ bot.py
@@ -6,7 +6,8 @@ bot = telebot.TeleBot("1763935068:AAEw63baPA3mul-18P5SzkE7tUxfqEWSjT4")
 
 @bot.message_handler(commands=["start"])
 def satart_message(message):
-    bot.send_message(message.chat.id, f"Здравствуй {message.from_user.first_name}!\n\n{start()}", reply_markup=keyboard1())
+    bot.send_message(message.chat.id, f"Здравствуй {message.from_user.first_name}!\n\n{start()}",
+                     reply_markup=keyboard1())
 
 
 @bot.message_handler(commands=["addbot"])
@@ -39,8 +40,12 @@ def answer_start_message(message):
         bot.send_message(message.chat.id, manual())
     elif message.text == "Помощь":
         bot.send_message(message.chat.id, help())
+    elif message.text == "Привет":
+        bot.send_message(message.chat.id, f"Здравствуй {message.from_user.first_name}!\n\n{start()}",
+                         reply_markup=keyboard1())
     else:
-        bot.send_message(message.chat.id, "Я не знаю таких команд. Пропешите /help, чтобы посмотреть существующие команды.")
+        bot.send_message(message.chat.id, "Я не знаю таких команд. Пропешите /help, "
+                                          "чтобы посмотреть существующие команды.")
 
 
 print("Бот запущен!!!")
