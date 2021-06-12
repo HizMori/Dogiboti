80dfa16a2d976f1240cd07be197a5482533cefab
Katyusha <74827754+HizMori@users.noreply.github.com>
2021-06-12 15:44:07 +0500

    bot

--- bot.py
+++ bot.py
@@ -8,12 +8,14 @@ def keyboard1():
     button2 = types.KeyboardButton("Инструкции")
     button3 = types.KeyboardButton("Помощь")
     keyboard1.add(button1, button2, button3)
+    return keyboard1
 
 
 def keyboard2():
-    keyboard2 = types.ReplyKeyboardMarkup()
+    keyboard2 = types.ReplyKeyboardMarkup(row_width=1)
     button1 = types.KeyboardButton("Отменить")
     keyboard2.add(button1)
+    return keyboard2
 
 
 @bot.message_handler(commands=["start"])
